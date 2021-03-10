b27369036465d9d08453a80196e6dd6e332c97c2
hack.efeet@gmail.com <hack.efeet@gmail.com>
2021-03-09 19:33:54 -0600

    NUevos cambios para Tomcat EBS

--- elasticbeanstalk/tomcat_ebs/.platform/hooks/postdeploy/add-catalina-log-to-cloudwatchstream.sh
+++ elasticbeanstalk/tomcat_ebs/.platform/hooks/postdeploy/add-catalina-log-to-cloudwatchstream.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 head -9 /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.d/file_beanstalk.json > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.d/catalina-log.json
-tail -6 file_beanstalk.json >> catalina-log.json
+tail -6 file_beanstalk.json >> /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.d/catalina-log.json
 
-sed -i '7s/eb-engine.log/tomcat\/catalina.out/g' catalina-log.json
-sed -i '8s/eb-engine.log/tomcat\/catalina.out/g' catalina-log.json
+sed -i '7s/eb-engine.log/tomcat\/catalina.out/g' /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.d/catalina-log.json
+sed -i '8s/eb-engine.log/tomcat\/catalina.out/g' /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.d/catalina-log.json
 
 systemctl restart amazon-cloudwatch-agent
